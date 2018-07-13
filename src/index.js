56647b979fcd984e7d8d50ceb822a34c51658b3f
Anton Bondarev <anton.v.bondarev@gmail.com>
2018-07-13 23:08:36 +0300

    layout outlined

--- src/index.js
+++ src/index.js
@@ -1,8 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+	Route,
+	NavLink,
+	HashRouter
+} from 'react-router-dom';
+import Home from './pages/Home';
+import Detail from './pages/Detail';
+import User from './pages/User';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+function App () {
+   return (
+      <HashRouter>
+         <div className="App">
+            <h2>Fake-API-Output</h2>
+            <ul className="header">
+               <li><NavLink exact to="/">Home</NavLink></li>
+               <li><NavLink to="/detail">Detail</NavLink></li>
+               <li><NavLink to="/user">User</NavLink></li>
+            </ul>
+            <div className="content">
+               <Route exact path="/" component={Home} />
+               <Route path="/detail" component={Detail} />
+               <Route path="/user" component={User} />
+            </div>
+         </div>
+      </HashRouter>  
+   );
+}
+   
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+if(module.hot) {
+   module.hot.accept()
+}
